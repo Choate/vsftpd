4fd6e031e8fa7efa9d5d42d45e60801dd094f0b1
Choate <choate.yao@gmail.com>
2015-12-14 16:07:22 +0800

    fixed migration table name error

--- migrations/m150828_095054_init.php
+++ migrations/m150828_095054_init.php
@@ -1,12 +1,12 @@
 <?php
-
+use choate\vsftpd\models\User;
 use yii\db\Schema;
 use yii\db\Migration;
 
 class m150828_095054_init extends Migration
 {
     public function up() {
-        $this->createTable('%vsftpd_user', [
+        $this->createTable(User::tableName(), [
                 'id'       => 'pk',
                 'username' => Schema::TYPE_STRING . '(45) NOT NULL',
                 'password' => Schema::TYPE_STRING . '(45) NOT NULL',
@@ -14,13 +14,13 @@ class m150828_095054_init extends Migration
                 'setting'     => Schema::TYPE_TEXT . ' NULL',
             ]
         );
-        $this->createIndex('username', 'vsftpd_user', 'username', true);
+        $this->createIndex('username', User::tableName(), 'username', true);
 
         return true;
     }
 
     public function down() {
-        $this->dropTable('IF EXISTS `vsftpd_user`');
+        $this->dropTable('IF EXISTS '.User::tableName());
 
         return true;
     }
